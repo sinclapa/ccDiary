f66ba112a95994d6169fba791abd4848a0503b8a
email <paul.john.sinclair@gmail.com>
2024-11-03 20:48:29 +0000

    CCDIARY-3: Fix

--- src/ui/tests/pages/diaries/[id].spec.ts
+++ src/ui/tests/pages/diaries/[id].spec.ts
@@ -3,6 +3,7 @@ import { afterEach, beforeEach, describe, expect, it, vi, Mock, MockInstance } f
 import { createVuetify } from 'vuetify'
 import { useRoute } from 'vue-router'
 import { diaryAPI } from '@/services/modules/diaryService'
+import { diaryEntryAPI } from '@/services/modules/diaryEntryService'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { state } from '@/services/authentication/msalConfig'
@@ -17,6 +18,8 @@ const vuetify = createVuetify({
 describe('pages/diaries/[id].vue Implementation Test', () => {
   const diaryId : string = crypto.randomUUID()
   const diary = new Diary('June as a Snowman', 'Mr Puddle', 'Life out of the freezer', diaryId)
+  const minDate = new Date(0, 0, 1)
+  const maxDate = new Date(9999, 0, 1)
 
   // Mock route
   vi.mock('vue-router')
@@ -25,6 +28,8 @@ describe('pages/diaries/[id].vue Implementation Test', () => {
 
   let wrapper: VueWrapper
   let getDiarySpy: MockInstance
+  let getMinDateSpy: MockInstance
+  let getMaxDateSpy: MockInstance
 
   beforeEach(() => {
     state.isAuthenticated = false
@@ -33,6 +38,14 @@ describe('pages/diaries/[id].vue Implementation Test', () => {
     // Mock getDiary
     getDiarySpy = vi.spyOn(diaryAPI, 'getDiary',).mockReturnValue(
       new Promise( resolve => resolve(diary)
+    ))
+
+    getMinDateSpy = vi.spyOn(diaryEntryAPI, 'getMinDate',).mockReturnValue(
+      new Promise( resolve => resolve(minDate)
+    ))
+
+    getMaxDateSpy = vi.spyOn(diaryEntryAPI, 'getMaxDate',).mockReturnValue(
+      new Promise( resolve => resolve(maxDate)
     ))
 
     wrapper = mount(Component, {
@@ -52,6 +65,10 @@ describe('pages/diaries/[id].vue Implementation Test', () => {
   it('Display controller', async () => {
     expect(getDiarySpy).toHaveBeenCalledOnce()
     expect(getDiarySpy).toBeCalledWith(diaryId)
+    expect(getMinDateSpy).toHaveBeenCalledOnce()
+    expect(getMinDateSpy).toBeCalledWith(diaryId)
+    expect(getMaxDateSpy).toHaveBeenCalledOnce()
+    expect(getMaxDateSpy).toBeCalledWith(diaryId)
     expect(wrapper.text()).toMatch(diary.title)
     expect(wrapper.text()).toMatch(diary.author)
     expect(wrapper.html()).not.toMatch('Add Entry')
