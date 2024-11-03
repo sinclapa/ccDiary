7af38b0fe196ec920653fd8aab8e876bca0dba28
email <paul.john.sinclair@gmail.com>
2024-11-03 19:28:35 +0000

    CCDIARY-3: Fix

--- src/ui/tests/app.spec.ts
+++ src/ui/tests/app.spec.ts
@@ -1,5 +1,5 @@
 import { mount, VueWrapper } from '@vue/test-utils'
-import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it } from 'vitest'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
--- src/ui/tests/stores/app.spec.ts
+++ src/ui/tests/stores/app.spec.ts
@@ -1,5 +1,5 @@
 import { useAppStore } from '@/stores/app'
-import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { describe, expect, it } from 'vitest'
 
 describe('App Store', () => {
   it('Load', async () => {
