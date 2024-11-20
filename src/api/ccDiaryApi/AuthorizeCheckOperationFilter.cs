ef7a02ada1dcb1063b1c8352fa6e9f0b1bfdd416
email <paul.john.sinclair@gmail.com>
2024-11-20 00:42:24 +0000

    CCDIARY-3: Add import unit tests

--- src/api/ccDiaryApi/AuthorizeCheckOperationFilter.cs
+++ src/api/ccDiaryApi/AuthorizeCheckOperationFilter.cs
@@ -4,10 +4,12 @@
 
 namespace ccDiaryApi
 {
+    using System.Diagnostics.CodeAnalysis;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.OpenApi.Models;
     using Swashbuckle.AspNetCore.SwaggerGen;
 
+    [ExcludeFromCodeCoverage]
     public class AuthorizeCheckOperationFilter : IOperationFilter
     {
         public void Apply(OpenApiOperation operation, OperationFilterContext context)
---
+++ src/api/ccDiaryApiTest/Integration/DiaryArchiveIntegrationTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using ccDiaryApi.Data.Model;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ccDiaryApiTest.Integration
+{
+    [TestClass]
+    public class DiaryArchiveIntegrationTests
+    {
+        private HttpClient _httpClient = new CustomWebApplicationFactory<Program>().CreateDefaultClient();
+
+        [TestInitialize]
+        public void CreateHttpClient()
+        {
+            _httpClient = new CustomWebApplicationFactory<Program>().CreateDefaultClient();
+        }
+
+        [TestMethod]
+        public async Task ImportNew()
+        {
+            // Arrange
+            var archiveDiary = CreateArchiveDiary();
+
+            // Act
+            var response = await _httpClient.PostAsJsonAsync($"api/v1/DiaryArchive/Import", archiveDiary);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var result = await response.Content.ReadFromJsonAsync<DiaryDTO>();
+            Assert.IsNotNull(result);
+            Assert.AreEqual(archiveDiary.Diary.DiaryId, result.DiaryId);
+            Assert.AreEqual(archiveDiary.Diary.Title, result.Title);
+            Assert.AreEqual(archiveDiary.Diary.Author, result.Author);
+            Assert.AreEqual(archiveDiary.Diary.Description, result.Description);
+
+            // Validate entries
+            var responeEntries = await _httpClient.GetAsync($"api/v1/DiaryEntry/GetDiaryEntries/{archiveDiary.Diary.DiaryId}");
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var resultEntries = await responeEntries.Content.ReadFromJsonAsync<IEnumerable<DiaryEntryDTO>>();
+            Assert.IsNotNull(resultEntries);
+            Assert.AreEqual(3, resultEntries.Count());
+            Assert.AreEqual(archiveDiary.DiaryEntries[0].Entry, resultEntries.ElementAt(0).Entry);
+            Assert.AreEqual(archiveDiary.DiaryEntries[1].Entry, resultEntries.ElementAt(1).Entry);
+            Assert.AreEqual(archiveDiary.DiaryEntries[2].Entry, resultEntries.ElementAt(2).Entry);
+        }
+
+        [TestMethod]
+        public async Task ImportUpdate()
+        {
+            // Arrange
+            var archiveDiary = CreateArchiveDiary();
+            await _httpClient.PostAsJsonAsync($"api/v1/DiaryArchive/Import", archiveDiary);
+            archiveDiary.Diary.Title = "Age of computers";
+            archiveDiary.DiaryEntries[1].Entry = "International Business Machines";
+
+            // Act
+            var response = await _httpClient.PostAsJsonAsync($"api/v1/DiaryArchive/Import", archiveDiary);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var result = await response.Content.ReadFromJsonAsync<DiaryDTO>();
+            Assert.IsNotNull(result);
+            Assert.AreEqual(archiveDiary.Diary.DiaryId, result.DiaryId);
+            Assert.AreEqual(archiveDiary.Diary.Title, result.Title);
+            Assert.AreEqual(archiveDiary.Diary.Author, result.Author);
+            Assert.AreEqual(archiveDiary.Diary.Description, result.Description);
+
+            // Validate entries
+            var responeEntries = await _httpClient.GetAsync($"api/v1/DiaryEntry/GetDiaryEntries/{archiveDiary.Diary.DiaryId}");
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var resultEntries = await responeEntries.Content.ReadFromJsonAsync<IEnumerable<DiaryEntryDTO>>();
+            Assert.IsNotNull(resultEntries);
+            Assert.AreEqual(3, resultEntries.Count());
+            Assert.AreEqual(archiveDiary.DiaryEntries[0].Entry, resultEntries.ElementAt(0).Entry);
+            Assert.AreEqual(archiveDiary.DiaryEntries[1].Entry, resultEntries.ElementAt(1).Entry);
+            Assert.AreEqual(archiveDiary.DiaryEntries[2].Entry, resultEntries.ElementAt(2).Entry);
+        }
+
+        private static DiaryArchiveDTO CreateArchiveDiary()
+        {
+            var diary = new DiaryDTO { Author = "Paul John", Title = "History of computers", Description = "Computers from ancient time to digital era", DiaryId = Guid.NewGuid() };
+            var diaryEntries = new List<DiaryEntryDTO>
+            {
+                new DiaryEntryDTO { Date = new DateTime(2024, 11, 19, 10, 15, 0, DateTimeKind.Utc), DiaryId = diary.DiaryId, Entry = "Spectrum", Location = "Glasgow", DiaryEntryId = Guid.NewGuid() },
+                new DiaryEntryDTO { Date = new DateTime(2024, 11, 19, 14, 25, 0, DateTimeKind.Utc), DiaryId = diary.DiaryId, Entry = "IBM", Location = "New York", DiaryEntryId = Guid.NewGuid() },
+                new DiaryEntryDTO { Date = new DateTime(2024, 11, 20, 8, 18, 0, DateTimeKind.Utc), DiaryId = diary.DiaryId, Entry = "Acorn", Location = "Manchester", DiaryEntryId = Guid.NewGuid() },
+            };
+            var archiveDiary = new DiaryArchiveDTO { Diary = diary, DiaryEntries = diaryEntries };
+            return archiveDiary;
+        }
+
+    }
+}
